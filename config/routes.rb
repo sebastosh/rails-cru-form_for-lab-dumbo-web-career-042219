b005db239991a945b98becec51d514b25473b1da
sebastosh <sebastosh@gmail.com>
2019-05-20 14:54:27 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,43 @@
 Rails.application.routes.draw do
+  # get 'songs/index'
+  #
+  # get 'songs/new'
+  #
+  # get 'songs/create'
+  #
+  # get 'songs/show'
+  #
+  # get 'songs/edit'
+  #
+  # get 'songs/update'
+  #
+  # get 'genres/index'
+  #
+  # get 'genres/new'
+  #
+  # get 'genres/create'
+  #
+  # get 'genres/show'
+  #
+  # get 'genres/edit'
+  #
+  # get 'genres/update'
+  #
+  # get 'artists/index'
+  #
+  # get 'artists/new'
+  #
+  # get 'artists/create'
+  #
+  # get 'artists/show'
+  #
+  # get 'artists/edit'
+  #
+  # get 'artists/update'
+
+  resources :artists
+    resources :songs
+       resources :genres
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
